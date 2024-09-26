77a0bbeba2517c6f52ed172004a8117da06623e7
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-09-25 21:41:33 -0400

    Create Lab1_part4.py

---
+++ Lab#1/Lab1_part4.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+LedPin1 = 11 # pin11
+LedPin2 = 13 # pin11
+
+GPIO.setmode(GPIO.BOARD)       # Number pins according to physical location
+GPIO.setup(LedPin1, GPIO.OUT)   # Set pin mode as output
+GPIO.output(LedPin1, GPIO.HIGH) # Set pin to high (+3.3V) to turn LED off 
+GPIO.setup(LedPin2, GPIO.OUT)   # Set pin mode as output
+GPIO.output(LedPin2, GPIO.HIGH) # Set pin to high (+3.3V) to turn LED off 
+A = LedPin1
+try:
+	while True:
+    GPIO.output(A, GPIO.LOW) 
+		time.sleep()
+		GPIO.output(LedPin1, GPIO.HIGH)
+		GPIO.output(LedPin2, GPIO.LOW)  
+		time.sleep(1)
+    
+except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the next two lines will be executed:
+	GPIO.output(LedPin1, GPIO.HIGH)     # turn LED off
+	GPIO.output(LedPin2, GPIO.HIGH)     # turn LED off
+	GPIO.cleanup()                     # Release resource
