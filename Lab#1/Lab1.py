ac9d0005fb13aae159cb8134fb583bec795604b3
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-09-25 23:12:20 -0400

    Update Lab1.py

--- Lab#1/Lab1.py
+++ Lab#1/Lab1.py
@@ -3,6 +3,29 @@
 #Blinking LED
 #Modified by Pelin Kurtay
 
+# import RPi.GPIO as GPIO
+# import time
+
+# LedPin = 11 # pin11
+
+# GPIO.setmode(GPIO.BOARD)       # Number pins according to physical location
+# GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
+# GPIO.output(LedPin, GPIO.HIGH) # Set pin to high (+3.3V) to turn LED off 
+
+# try:
+# 	while True:
+# 		print ('...led on')
+# 		GPIO.output(LedPin, GPIO.LOW)  # LED on
+# 		time.sleep(5)
+# 		print ('led off...')
+# 		GPIO.output(LedPin, GPIO.HIGH) # LED off
+# 		time.sleep(5)
+		
+# except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the next two lines will be executed:
+# 	GPIO.output(LedPin, GPIO.HIGH)     # turn LED off
+# 	GPIO.cleanup()                     # Release resource
+	
+	
 import RPi.GPIO as GPIO
 import time
 
@@ -10,19 +33,18 @@ LedPin = 11 # pin11
 
 GPIO.setmode(GPIO.BOARD)       # Number pins according to physical location
 GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
-GPIO.output(LedPin, GPIO.HIGH) # Set pin to high (+3.3V) to turn LED off 
+GPIO.output(LedPin, GPIO.HIGH) # Set pin to high (+3.3V) to turn LED on
 
 try:
 	while True:
 		print ('...led on')
-		GPIO.output(LedPin, GPIO.LOW)  # LED on
+		GPIO.output(LedPin, GPIO.LOW)  # LED off
 		time.sleep(5)
 		print ('led off...')
-		GPIO.output(LedPin, GPIO.HIGH) # LED off
+		GPIO.output(LedPin, GPIO.HIGH) # LED on
 		time.sleep(5)
 		
 except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the next two lines will be executed:
-	GPIO.output(LedPin, GPIO.HIGH)     # turn LED off
+	GPIO.output(LedPin, GPIO.HIGH)     # turn LED on
 	GPIO.cleanup()                     # Release resource
-	
 	
