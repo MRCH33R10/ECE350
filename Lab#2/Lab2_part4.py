21397dae5969034404618ae049a1807c6c5f219c
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-08 21:22:15 -0400

    Update Lab2_part4.py

--- Lab#2/Lab2_part4.py
+++ Lab#2/Lab2_part4.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 
-tmeARR = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
+tmeARR = [1.00, 0.95, 0.90, 0.85, 0.80, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05, 0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00]
 
 #(LedPin1, BzrPin, LedPin2)
 OutLowArr = [11, 12]
@@ -22,14 +22,7 @@ try:
 				time.sleep(i/200)
 				GPIO.output(OutLowArr[1], GPIO.LOW)
 				GPIO.output(OutLowArr[0], GPIO.LOW)
-				time.sleep((1-i)/200)
-			for x in range(200): #100Hz
-				GPIO.output(OutLowArr[1], GPIO.HIGH)
-				GPIO.output(OutLowArr[0], GPIO.HIGH)
-				time.sleep((1-i)/200)
-				GPIO.output(OutLowArr[1], GPIO.LOW)
-				GPIO.output(OutLowArr[0], GPIO.LOW)
-				time.sleep(i/200)				
+				time.sleep((1-i)/200)				
         
 except KeyboardInterrupt:               #set up keyboard interrupt ctrl C
 	for y in OutLowArr:  
