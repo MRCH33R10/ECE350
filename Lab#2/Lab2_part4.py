d6a348e6ef72b12893c32376af594f585d16e986
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-08 21:09:36 -0400

    Update Lab2_part4.py

--- Lab#2/Lab2_part4.py
+++ Lab#2/Lab2_part4.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 
-tmeARR = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+tmeARR = [1, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.55, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0]
 
 #(LedPin1, BzrPin, LedPin2)
 OutLowArr = [11, 12]
@@ -15,7 +15,7 @@ for i in OutLowArr:
 
 try:
 	while True:
-		for i in range(1,100):
+		for i in tmeArr:
 			for x in range(200): #100Hz
 				GPIO.output(OutLowArr[1], GPIO.HIGH)
 				GPIO.output(OutLowArr[0], GPIO.HIGH)
