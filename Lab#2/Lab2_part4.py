47f9a862a5d44859196dc99d45b48c1831ae4b02
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-08 22:23:37 -0400

    Update Lab2_part4.py

--- Lab#2/Lab2_part4.py
+++ Lab#2/Lab2_part4.py
@@ -20,10 +20,14 @@ try:
 			for w in range(1):
 				GPIO.output(OutLowArr[1], GPIO.HIGH)
 				GPIO.output(OutLowArr[0], GPIO.HIGH)
-				time.sleep(i/200000)
+				time.sleep(i/400000)
 				GPIO.output(OutLowArr[1], GPIO.LOW)
 				GPIO.output(OutLowArr[0], GPIO.LOW)
-				time.sleep((1000-i)/200000)
+				time.sleep((1000-i)/400000)
+				GPIO.output(OutLowArr[1], GPIO.HIGH)
+				time.sleep(i/400000)
+				GPIO.output(OutLowArr[1], GPIO.LOW)
+				time.sleep((1000-i)/400000)
 		GPIO.output(OutLowArr[1], GPIO.HIGH)
 		GPIO.output(OutLowArr[0], GPIO.HIGH)
 		for i in range(1,999):
