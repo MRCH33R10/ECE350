98acc1a3e5224f0db8b2b0bbf8561393095cd48e
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-08 21:44:41 -0400

    Update Lab2_part4.py

--- Lab#2/Lab2_part4.py
+++ Lab#2/Lab2_part4.py
@@ -15,14 +15,14 @@ for i in OutLowArr:
 
 try:
 	while True:
-		for i in range(1,99):
+		for i in range(1,9):
 			for x in range(100):
 				GPIO.output(OutLowArr[1], GPIO.HIGH)
 				GPIO.output(OutLowArr[0], GPIO.HIGH)
-				time.sleep(i/20000)
+				time.sleep(i/2000)
 				GPIO.output(OutLowArr[1], GPIO.LOW)
 				GPIO.output(OutLowArr[0], GPIO.LOW)
-				time.sleep((100-i)/20000)				
+				time.sleep((10-i)/2000)				
         
 except KeyboardInterrupt:               #set up keyboard interrupt ctrl C
 	for y in OutLowArr:  
