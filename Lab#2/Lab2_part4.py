96830fa2c277a7ecd246f5cf1e95983ac5b95075
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-08 21:46:01 -0400

    Update Lab2_part4.py

--- Lab#2/Lab2_part4.py
+++ Lab#2/Lab2_part4.py
@@ -16,7 +16,7 @@ for i in OutLowArr:
 try:
 	while True:
 		for i in range(1,9):
-			for x in range(100):
+			for x in range(50):
 				GPIO.output(OutLowArr[1], GPIO.HIGH)
 				GPIO.output(OutLowArr[0], GPIO.HIGH)
 				time.sleep(i/2000)
