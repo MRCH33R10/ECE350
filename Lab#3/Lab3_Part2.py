f13aa54ca3fb3b86aa2b5bddcecff476f51208f2
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-10-17 21:46:04 -0400

    Update Lab3_Part2.py

--- Lab#3/Lab3_Part2.py
+++ Lab#3/Lab3_Part2.py
@@ -7,23 +7,20 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servopin, GPIO.OUT)
 p = GPIO.PWM(servopin,50)
 p.start(0)
-time.sleep(1)
+time.sleep(2)
 
 try: 
 	while(True):
-		for i in range(0,181,10):
+		p.start(12.5)
+		time.sleep(5)
+		for i in range(0,181,18):
 			print('i=',i)
-			p.ChangeDutyCycle(2.5 + 10 * i / 180)
-			time.sleep(0.2)
-			p.ChangeDutyCycle(0)
-			time.sleep(0.5)
-
-		for i in range(180,0,-10):
+			p.ChangeDutyCycle(2.5 + 5.5 * i / 180)
+			time.sleep(0.1)
+		for i in range(180,0,-18):
 			print('........i=',i)
-			p.ChangeDutyCycle(2.5 + 10 * i / 180)
-			time.sleep(0.2)
-			p.ChangeDutyCycle(0)
-			time.sleep(0.5)
+			p.ChangeDutyCycle(2.5 + 5.5 * i / 180)
+			time.sleep(0.1)
 
 except KeyboardInterrupt:
 	p.stop()
