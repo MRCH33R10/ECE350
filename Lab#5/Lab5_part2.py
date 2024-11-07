9501b03fe2fd2f163b4f3cb79ccd1b072e672af3
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-11-07 00:07:39 -0500

    Update Lab5_part2.py

--- Lab#5/Lab5_part2.py
+++ Lab#5/Lab5_part2.py
@@ -48,7 +48,10 @@ import time
 
 RoAPin = 16    # pin11
 RoBPin = 18    # pin12
-BtnPin = 13    # Button Pin
+IN1 = 11    # pin11  
+IN2 = 12
+IN3 = 13
+IN4 = 15
 
 globalCounter = 0
 
@@ -59,8 +62,33 @@ Current_RoB_Status = 0
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(RoAPin, GPIO.IN)    # input mode
-	GPIO.setup(RoBPin, GPIO.IN)
-
+	GPIO.setup(RoBPin, GPIO.IN)	
+	GPIO.setup(IN1, GPIO.OUT)      # Set pin's mode is output  
+	GPIO.setup(IN2, GPIO.OUT)
+	GPIO.setup(IN3, GPIO.OUT)
+	GPIO.setup(IN4, GPIO.OUT)
+def stop():
+	setStep(0, 0, 0, 0)
+def forward(delay, steps):    
+	for i in range(0, steps):
+		setStep(1, 0, 0, 0)
+		time.sleep(delay)
+		setStep(0, 1, 0, 0)
+		time.sleep(delay)
+		setStep(0, 0, 1, 0)
+		time.sleep(delay)
+		setStep(0, 0, 0, 1)
+		time.sleep(delay)
+def backward(delay, steps):
+	for i in range(0, steps):
+		setStep(0, 0, 0, 1)
+		time.sleep(delay)
+		setStep(0, 0, 1, 0)
+		time.sleep(delay)
+		setStep(0, 1, 0, 0)
+		time.sleep(delay)
+		setStep(1, 0, 0, 0)
+		time.sleep(delay)
 def rotaryDeal():
 	global flag
 	global Last_RoB_Status
@@ -74,8 +102,17 @@ def rotaryDeal():
 		flag = 0
 		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
 			globalCounter = globalCounter - 1
+			backward(0.003, 512)  # 512 steps --- 360 angle  
+			stop()                 # stop  
 		if (Last_RoB_Status == 1) and (Current_RoB_Status == 0):
 			globalCounter = globalCounter + 1
+			forward(0.003, 512)  # 512 steps --- 360 angle  
+			stop()                 # stop  			
+	else:
+		if globalCounter > 0:
+			globalCounter -= 1
+		elif globalCounter < 0:
+			globalCounter += 1
 
 
 def loop():
