2c0b9a6482c8f254f46cb5acb609a201afb6bd1e
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-11-07 00:08:52 -0500

    Update Lab5_part2.py

--- Lab#5/Lab5_part2.py
+++ Lab#5/Lab5_part2.py
@@ -59,6 +59,11 @@ flag = 0
 Last_RoB_Status = 0
 Current_RoB_Status = 0
 
+def setStep(w1, w2, w3, w4):
+	GPIO.output(IN1, w1)
+	GPIO.output(IN2, w2)
+	GPIO.output(IN3, w3)
+	GPIO.output(IN4, w4)
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(RoAPin, GPIO.IN)    # input mode
