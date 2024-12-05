2272349be36aecde431e961a439cfd949047ae1b
MRCH33R10 <116801760+MRCH33R10@users.noreply.github.com>
2024-12-05 08:42:26 -0500

    Update mtr.py

--- mtr.py
+++ mtr.py
@@ -9,10 +9,10 @@ GPIO.setwarnings(False)
 # We name all the pins on BOARD mode
 GPIO.setmode(GPIO.BOARD)
 # Set an output for the PWM Signal
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 # Set up the PWM on pin #16 at 50Hz
-pwm = GPIO.PWM(16, 50)
+pwm = GPIO.PWM(15, 50)
 pwm.start(0) # Start the servo with 0 duty cycle ( at 0 deg position )
 pwm.ChangeDutyCycle(5) # Tells the servo to turn to the left ( -90 deg position )
 sleep(0.5) # Tells the servo to Delay for 5sec
